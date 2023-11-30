53ebe61d67ca6a52e759a333431d7d043ee4833f
Тима <novii.comp@mail.ru>
2023-11-30 15:39:34 +0300

    проверка 1

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -63,19 +62,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.IblSucess = new System.Windows.Forms.Label();
             this.IblError = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(676, 391);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(270, 70);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "посчитать";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -158,25 +151,26 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(747, 476);
+            this.textBox5.Location = new System.Drawing.Point(799, 380);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(137, 27);
+            this.textBox5.Size = new System.Drawing.Size(174, 27);
             this.textBox5.TabIndex = 11;
             this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(836, 312);
+            this.textBox7.Location = new System.Drawing.Point(799, 312);
             this.textBox7.Multiline = true;
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(137, 27);
+            this.textBox7.Size = new System.Drawing.Size(174, 27);
             this.textBox7.TabIndex = 13;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(634, 315);
+            this.label6.Location = new System.Drawing.Point(594, 312);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(196, 16);
             this.label6.TabIndex = 14;
@@ -185,7 +179,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(673, 479);
+            this.label7.Location = new System.Drawing.Point(725, 383);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(68, 16);
             this.label7.TabIndex = 15;
@@ -194,7 +188,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(658, 350);
+            this.label8.Location = new System.Drawing.Point(618, 350);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(172, 16);
             this.label8.TabIndex = 16;
@@ -212,11 +206,12 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(836, 345);
+            this.textBox6.Location = new System.Drawing.Point(799, 345);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(137, 27);
+            this.textBox6.Size = new System.Drawing.Size(174, 27);
             this.textBox6.TabIndex = 18;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
             // textBox11
             // 
@@ -226,7 +221,6 @@
             this.textBox11.Name = "textBox11";
             this.textBox11.Size = new System.Drawing.Size(86, 32);
             this.textBox11.TabIndex = 24;
-            this.textBox11.Text = "52.20";
             this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
             // 
             // label10
@@ -293,15 +287,15 @@
             "92p"});
             this.comboBox1.Location = new System.Drawing.Point(799, 242);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(174, 24);
             this.comboBox1.TabIndex = 29;
-            this.comboBox1.Text = "Не выбран";
+            this.comboBox1.Text = "нажмите для выбора";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(634, 282);
+            this.label16.Location = new System.Drawing.Point(658, 282);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(132, 16);
             this.label16.TabIndex = 30;
@@ -314,11 +308,12 @@
             "1",
             "2",
             "3"});
-            this.comboBox2.Location = new System.Drawing.Point(772, 279);
+            this.comboBox2.Location = new System.Drawing.Point(799, 282);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.Size = new System.Drawing.Size(174, 24);
             this.comboBox2.TabIndex = 31;
-            this.comboBox2.Text = "Не выбран";
+            this.comboBox2.Text = "нажмите для выбора";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // textBox12
             // 
@@ -328,17 +323,15 @@
             this.textBox12.Name = "textBox12";
             this.textBox12.Size = new System.Drawing.Size(86, 32);
             this.textBox12.TabIndex = 32;
-            this.textBox12.Text = "50.20";
             // 
             // textBox14
             // 
             this.textBox14.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox14.Location = new System.Drawing.Point(766, 81);
+            this.textBox14.Location = new System.Drawing.Point(766, 80);
             this.textBox14.Multiline = true;
             this.textBox14.Name = "textBox14";
             this.textBox14.Size = new System.Drawing.Size(86, 32);
             this.textBox14.TabIndex = 34;
-            this.textBox14.Text = "51.1";
             // 
             // textBox13
             // 
@@ -348,16 +341,16 @@
             this.textBox13.Name = "textBox13";
             this.textBox13.Size = new System.Drawing.Size(86, 32);
             this.textBox13.TabIndex = 33;
-            this.textBox13.Text = "49.9";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1036, 296);
+            this.button1.Location = new System.Drawing.Point(1043, 282);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(270, 207);
             this.button1.TabIndex = 35;
             this.button1.Text = "Пробить чек";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -392,22 +385,63 @@
             this.IblError.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             this.IblError.Click += new System.EventHandler(this.label18_Click);
             // 
-            // dataGridView1
+            // button4
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(-28, -57);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 39;
+            this.button4.Location = new System.Drawing.Point(578, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 75);
+            this.button4.TabIndex = 40;
+            this.button4.Text = "обновить цены";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(872, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 111);
+            this.button5.TabIndex = 41;
+            this.button5.Text = "изменить цены";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1289, 130);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 43);
+            this.button6.TabIndex = 42;
+            this.button6.Text = "обновить остаток";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(375, 92);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 44;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1276, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 53);
+            this.button2.TabIndex = 45;
+            this.button2.Text = "изменить остаток";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1781, 559);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.IblError);
             this.Controls.Add(this.IblSucess);
             this.Controls.Add(this.button3);
@@ -441,18 +475,15 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
             this.Name = "Form1";
-            this.Text = "1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -487,7 +518,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label IblSucess;
         private System.Windows.Forms.Label IblError;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,13 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp1.Data;
 
@@ -31,9 +24,27 @@ namespace WindowsFormsApp1
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
 
+        {
+            MySqlConnection con = Connection.GetConnection();
+            MySqlCommand type = new MySqlCommand("SELECT * FROM type", con);
+            MySqlDataAdapter tip = new MySqlDataAdapter(type);
+            DataTable dt = new DataTable();
+            tip.Fill(dt);
+            comboBox1.DataSource = dt;
+            comboBox1.DisplayMember = "type_name";
+            comboBox1.ValueMember = "id";
+            MySqlCommand colonka = new MySqlCommand("SELECT * FROM colonka", con);
+            MySqlDataAdapter number = new MySqlDataAdapter(colonka);
+            DataTable cl = new DataTable();
+            number.Fill(cl);
+            comboBox2.DataSource = cl;
+            comboBox2.DisplayMember = "number";
+            comboBox2.ValueMember = "id";
         }
+
+
+
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
@@ -57,8 +68,7 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox7.Text = ("a");
-            textBox6.Text = ("b");
+       
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
@@ -78,7 +88,9 @@ namespace WindowsFormsApp1
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            MySqlConnection con = Connection.GetConnection();
+            MySqlCommand cena = new MySqlCommand("SELECT * FROM ostatok WHERE id = 1", con);
+            MySqlDataReader reader = cena.ExecuteReader();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -89,7 +101,7 @@ namespace WindowsFormsApp1
                 IblError.Visible = false;
                 IblSucess.Visible = true;
             }
-            catch (Exception) 
+            catch (Exception)
             {
                 IblSucess.Visible = false;
                 IblError.Visible = true;
@@ -106,5 +118,248 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = Connection.GetConnection();
+            MySqlCommand cena = new MySqlCommand("SELECT * FROM ostatok WHERE id = 1", con);
+            MySqlDataReader reader = cena.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                var id1 = reader.GetInt32(0);
+                var price = reader.GetString(1);
+                textBox11.Text = price;
+                reader.Close();
+
+            }
+            MySqlCommand cena2 = new MySqlCommand("SELECT * FROM ostatok WHERE id = 2", con);
+            MySqlDataReader reader2 = cena2.ExecuteReader();
+            if (reader2.HasRows)
+            {
+                reader2.Read();
+                var id1 = reader2.GetInt32(0);
+                var price = reader2.GetString(1);
+                textBox14.Text = price;
+                reader2.Close();
+            }
+            MySqlCommand cena3 = new MySqlCommand("SELECT * FROM ostatok WHERE id = 3", con);
+            MySqlDataReader reader3 = cena3.ExecuteReader();
+            if (reader3.HasRows)
+            {
+                reader3.Read();
+                var id1 = reader3.GetInt32(0);
+                var price = reader3.GetString(1);
+                textBox13.Text = price;
+                reader3.Close();
+            }
+            MySqlCommand cena4 = new MySqlCommand("SELECT * FROM ostatok WHERE id = 4", con);
+            MySqlDataReader reader4 = cena4.ExecuteReader();
+            if (reader4.HasRows)
+            {
+                reader4.Read();
+                var id1 = reader4.GetInt32(0);
+                var price = reader4.GetString(1);
+                textBox12.Text = price;
+                reader4.Close();
+            }
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           MySqlConnection con = Connection.GetConnection();
+
+            update_cena(1,Convert.ToDouble(textBox11.Text), con);
+            update_cena(2, Convert.ToDouble(textBox14.Text), con);
+            update_cena(3, Convert.ToDouble(textBox13.Text), con);
+            update_cena(4, Convert.ToDouble(textBox12.Text), con);
+            con.Close();
+          
+
+           MessageBox.Show("Цена успешно обновлена.");
+        }
+        private void update_cena(int id, double price, MySqlConnection con)
+        {
+            string updatecena92 = "UPDATE ostatok SET price = @price WHERE id = @id";
+            MySqlCommand upcena92 = new MySqlCommand(updatecena92, con);
+            upcena92.Parameters.Add(new MySqlParameter("@price", price  ));
+            upcena92.Parameters.Add(new MySqlParameter("@id", id));
+            upcena92.ExecuteNonQuery();
+            
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedValue.ToString() == "1")
+                {
+                    double c = Convert.ToDouble(textBox11.Text);
+                    double u = Convert.ToDouble(textBox7.Text);
+                    double i = u * c;
+                    textBox6.Text = i.ToString();
+                    textBox5.Text = textBox6.Text.ToString();
+                }
+                if (comboBox1.SelectedValue.ToString() == "2")
+                {
+                    double c = Convert.ToDouble(textBox14.Text);
+                    double u = Convert.ToDouble(textBox7.Text);
+                    double i = u * c;
+                    textBox6.Text = i.ToString();
+                    textBox5.Text = textBox6.Text.ToString();
+                }
+                if (comboBox1.SelectedValue.ToString() == "3")
+                {
+                    double c = Convert.ToDouble(textBox13.Text);
+                    double u = Convert.ToDouble(textBox7.Text);
+                    double i = u * c;
+                    textBox6.Text = i.ToString();
+                    textBox5.Text = textBox6.Text.ToString();
+                }
+                if (comboBox1.SelectedValue.ToString() == "4")
+                {
+                    double c = Convert.ToDouble(textBox12.Text);
+                    double u = Convert.ToDouble(textBox7.Text);
+                    double i = u * c;
+                    textBox6.Text = i.ToString();
+                    textBox5.Text = textBox6.Text.ToString();
+                }
+            } 
+            catch { };
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = Connection.GetConnection();
+            MySqlCommand ost = new MySqlCommand("SELECT * FROM ostatok WHERE id = 1", con);
+            MySqlDataReader readerost = ost.ExecuteReader();
+            if (readerost.HasRows)
+            {
+                readerost.Read();
+                var id1 = readerost.GetInt32(0);
+                var ostatok = readerost.GetString(3);
+                textBox1.Text = ostatok;
+                readerost.Close();
+
+            }
+            MySqlCommand ost2 = new MySqlCommand("SELECT * FROM ostatok WHERE id = 2", con);
+            MySqlDataReader readerost2 = ost2.ExecuteReader();
+            if (readerost2.HasRows)
+            {
+                readerost2.Read();
+                var id1 = readerost2.GetInt32(0);
+                var ostatok = readerost2.GetString(3);
+                textBox2.Text = ostatok;
+                readerost2.Close();
+            }
+            MySqlCommand ost3 = new MySqlCommand("SELECT * FROM ostatok WHERE id = 3", con);
+            MySqlDataReader readerost3 = ost3.ExecuteReader();
+            if (readerost3.HasRows)
+            {
+                readerost3.Read();
+                var id1 = readerost3.GetInt32(0);
+                var ostatok = readerost3.GetString(3);
+                textBox3.Text = ostatok;
+                readerost3.Close();
+            }
+            MySqlCommand ost4 = new MySqlCommand("SELECT * FROM ostatok WHERE id = 4", con);
+            MySqlDataReader readerost4 = ost4.ExecuteReader();
+            if (readerost4.HasRows)
+            {
+                readerost4.Read();
+                var id1 = readerost4.GetInt32(0);
+                var ostatok = readerost4.GetString(3);
+                textBox4.Text = ostatok;
+                readerost4.Close();
+            }
+            con.Close();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var vidtopliva = "SELECT * FROM type";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MySqlConnection con = Connection.GetConnection();
+            MySqlCommand colonka = new MySqlCommand("SELECT * FROM colonka WHERE id = 1", con);
+            MySqlDataReader reader = colonka.ExecuteReader();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {  try
+            {
+                if (comboBox1.SelectedValue.ToString() == "1")
+                {
+                    double k = Convert.ToDouble(textBox11.Text);
+                    double j = Convert.ToDouble(textBox6.Text);
+                    double p = j / k;
+                    textBox7.Text = p.ToString();
+                }
+                if (comboBox1.SelectedValue.ToString() == "2")
+                {
+                    double k = Convert.ToDouble(textBox14.Text);
+                    double j = Convert.ToDouble(textBox6.Text);
+                    double p = j / k;
+                    textBox7.Text = p.ToString();
+                }
+                if (comboBox1.SelectedValue.ToString() == "3")
+                {
+                    double k = Convert.ToDouble(textBox13.Text);
+                    double j = Convert.ToDouble(textBox6.Text);
+                    double p = j / k;
+                    textBox7.Text = p.ToString();
+                }
+                if (comboBox1.SelectedValue.ToString() == "4")
+                {
+                    double k = Convert.ToDouble(textBox12.Text);
+                    double j = Convert.ToDouble(textBox6.Text);
+                    double p = j / k;
+                    textBox7.Text = p.ToString();
+                }
+            } catch { };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedValue.ToString() == "1")
+            {
+                string m = Convert.ToString(textBox7.Text);
+                string g = Convert.ToString(comboBox2.Text);
+                string j = Convert.ToString(textBox1.Text);
+                string p = Convert.ToString(textBox6.Text);
+                string o = Convert.ToString(textBox5.Text);
+                //m = label17.Text;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MySqlConnection con = Connection.GetConnection();
+
+            update_ost(1, Convert.ToDouble(textBox1.Text), con);
+            update_ost(2, Convert.ToDouble(textBox2.Text), con);
+            update_ost(3, Convert.ToDouble(textBox3.Text), con);
+            update_ost(4, Convert.ToDouble(textBox4.Text), con);
+            con.Close();
+
+
+            MessageBox.Show("Остаток успешно обновлен.");
+        }
+        private void update_ost(int id, double ostatok, MySqlConnection con)
+        {
+            string updateost = "UPDATE ostatok SET ostatok = @ostatok WHERE id = @id";
+            MySqlCommand upost = new MySqlCommand(updateost, con);
+            upost.Parameters.Add(new MySqlParameter("@ostatok", ostatok  ));
+            upost.Parameters.Add(new MySqlParameter("@id", id));
+            upost.ExecuteNonQuery();
+            
+       
+        }
     }
+
+
 }
